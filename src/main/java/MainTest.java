0d20114d0845b1d9f27f588617df744f6bd49b06
Dennis Jakobi <dennis.jakobi76@gmail.com>
2026-01-05 17:16:35 +0100

    replace ReentrantLockExample with ReadWriteLockExample in MainTest; add ReadWriteLockExample class

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -2,15 +2,31 @@ public class MainTest {
 
     public static void main(String[] args) throws InterruptedException {
 
-        ReentrantLockExample reentrantLock = new ReentrantLockExample();
+//        ReentrantLockExample reentrantLock = new ReentrantLockExample();
+//
+//        Thread t1 = new Thread(reentrantLock::increment, "Thread 1");
+//        Thread t2 = new Thread(reentrantLock::increment, "Thread 2");
+//
+//        t1.start();
+//        t2.start();
+//
+//        t1.join();
+//        t2.join();
 
-        Thread t1 = new Thread(reentrantLock::increment, "Thread 1");
-        Thread t2 = new Thread(reentrantLock::increment, "Thread 2");
+        ReadWriteLockExample readWriteLock = new ReadWriteLockExample();
 
-        t1.start();
-        t2.start();
+        Thread writerThread = new Thread(() -> readWriteLock.write(49), "Writer Thread");
+        Thread reader1 = new Thread(readWriteLock::read, "Reader 1");
+        Thread reader2 = new Thread(readWriteLock::read, "Reader 2");
 
-        t1.join();
-        t2.join();
+
+
+        writerThread.start();
+        reader1.start();
+        reader2.start();
+
+        writerThread.join();
+        reader1.join();
+        reader2.join();
     }
 }
